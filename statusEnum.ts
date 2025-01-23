cb891ba472145afdbdd79e6eefcded410cd634e0
HueXi <hu3xi0@gmail.com>
2025-01-23 03:10:28 -0800

    initial

---
+++ statusEnum.ts
@@ -0,0 +1,11 @@
+export enum EOStatus {
+  ACTIVE = "Active",
+  IN_PROGRESS = "Implementation in progress",
+  BLOCKED = "Blocked by lawsuit",
+  PARTIALLY_BLOCKED = "Partially blocked",
+  RESCINDED = "Rescinded",
+  AWAITING_REVIEW = "Awaiting Review",
+  UNCLEAR = "Unclear / Hard to implement",
+  COMPLETE = "Fully implemented"
+}
+
